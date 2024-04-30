f68be5cc83e62eb2caf588624421c53584a50882
Carl0sC <carlos.carrionb@outlook.com>
2024-04-30 16:52:07 -0500

    Primer comit

---
+++ PeliculasCalifi/Models/entidades/movies.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasCalifi.Models.entidades
+{
+    public class Movies
+    {
+        public int movieid { get; set; }
+      
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Tilte { get; set;}
+        
+        [DisplayFormat (DataFormatString = "{0:yyy/MM/dd}")]
+        public DateTime RelaseYears { get; set;}
+        public string Genre {  get; set;}
+        public string Description { get; set; }
+
+        public double AverageRating { get; set; }
+
+    }
+}
